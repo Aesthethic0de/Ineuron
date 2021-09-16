e5bf14e398c080bf49d2f157d31f28ff6c728dff
unknown <mrsingh.someet@gmail.com>
2021-09-16 10:14:52 +0530

    Final Perceptron

--- and.py
+++ and.py
@@ -1,13 +1,12 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data, save_model, save_plot
+import numpy as np
 import pandas as pd
 
-
-
 AND = {
-    "X1" : [0,0,1,1],
-    "X2" : [0,1,0,1],
-    "X3" : [0,0,0,1],
+    "x1" : [0,0,1,1],
+    "x2" : [0,1,0,1],
+    "y" : [0,0,0,1]
 }
 df = pd.DataFrame(AND)
 df
@@ -17,3 +16,7 @@ EPOCHS = 10
 model = Perceptron(eta = ETA, epochs = EPOCHS)
 model.fit(X,y)
 _ = model.total_loss()
+
+save_model(model, filename="and.model")
+save_plot(df, "and.png", model)
+
--- or.py
+++ or.py
@@ -1,13 +1,13 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data, save_model, save_plot
 import pandas as pd
 import numpy as np
 
 
 OR = {
-    "X1" : [0,0,1,1],
-    "X2" : [0,1,0,1],
-    "X3" : [0,1,1,1],
+    "x1" : [0,0,1,1],
+    "x2" : [0,1,0,1],
+    "y" : [0,1,1,1],
 }
 df = pd.DataFrame(OR)
 df
@@ -18,3 +18,5 @@ model = Perceptron(eta = ETA, epochs = EPOCHS)
 model.fit(X,y)
 _ = model.total_loss()
 
+save_model(model, filename="or.model")
+save_plot(df, "or.png", model)
